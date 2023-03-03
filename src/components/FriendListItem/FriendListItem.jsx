a3f004d000e33d4fce115c927591be4669bd6f71
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-03-03 17:56:32 +0200

    task3

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import clsx from "clsx";
 import css from './FriendListItem.module.css';
 
 
@@ -6,7 +7,7 @@ export const FriendListItem =({avatar , name , isOnline }) => {
     return (
     <li 
     className={css.item}>
-    <span className={css.status}>{isOnline}</span>
+    <span className={clsx(isOnline ? css.status : css.isOffline)}>{isOnline}</span>
     <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
     <p className={css.name}>{name}</p>
   </li>
