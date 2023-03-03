7f15b7bfda9244507bc9f84909cf45a735e77bc8
Anastasiia Herusova <nastia.herusova@gmail.com>
2023-03-03 17:23:23 +0200

    task3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+// import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import { FriendList } from 'components/FriendList/FriendList';
 
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
-import transactions from 'data/transactions.json';
+// import transactions from 'data/transactions.json';
 
 export const App = () => {
 return ( <div>
